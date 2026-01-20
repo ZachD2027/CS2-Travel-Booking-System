935a611bc535a2f4200ee2a2ea1124bae79add6f
Zach <zachary.dowling2027@gbwl.org>
2026-01-20 16:06:01 +0000

    Declared Instance Variables & Constructors

---
+++ SubmarineTrip.java
@@ -0,0 +1,23 @@
+public class SubmarineTrip {
+// 1. INSTANCE VARIABLES (data)
+private static int processFee = 250000; // class variable
+private String travelerName;
+private String destination;
+private int durationInHours;
+private boolean isAlive;
+private double totalCost;
+// 2. CONSTRUCTORS (default + parameterized)
+public SubmarineTrip() {
+    this.travelerName = "DefaultClient";
+    this.destination = "Titanic Wreck"
+    this.durationInHours =8;
+    this.isAlive = false;
+    this.totalCost.processFee * durationInDays; 
+}
+public SubmarineTrip(String name, String destination, int duration, boolean alive) {
+    this.travelerName = name;
+    this.totalCost = processFee * this.durationInDays;
+}
+
+
+}
