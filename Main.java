b1b41b1709e66bddee4d6a08e634b2a6b5e823f9
Zach <zachary.dowling2027@gbwl.org>
2026-01-27 16:04:21 +0000

    Finished

--- Main.java
+++ Main.java
@@ -1,6 +1,61 @@
+import java.util.Scanner;
+
 public class Main {
 
    public static void main(String []args) {
+
+      //Test out the cnstructor for Trip objects
       SubmarineTrip trip1 = new SubmarineTrip();
+      System.out.print(trip1);
+
+      Scanner input = new Scanner(System.in);
+      // LOOP for booking trips with client input
+      boolean continueBooking = true; // flag to control loop
+      while (continueBooking) {
+         // 1. Call static bookTravel method to create object
+         SubmarineTrip currentTrip = bookTrip(input);
+         // 2. Display the object's details
+         System.out.println(currentTrip);
+         // 3. Ask user if they want to continue booking
+         System.out.println("Do you want to book another Submarine trip? (y/n)");
+         String response = input.nextLine();
+         if ( response.equals("n") ) {
+            continueBooking = false; // stop the loop
+         }
+
+      }
+
+
+   } // end main method
+
+   // Methods don't have to just be for object classes!
+   // Write a reusable process for creating objects
+   public static SubmarineTrip bookTrip(Scanner scan) {
+      // 1. Create a blank travel object
+      SubmarineTrip newTrip = new SubmarineTrip();
+
+      // 2. Ask client for deatils
+      System.out.println("What is your name?");
+      String clientName = scan.nextLine();
+      System.out.println("Where do you want to go?");
+      String clientDestination = scan.nextLine();
+      System.out.println("How many days?");
+      int clientDuration = scan.nextInt();
+      System.out.println("Are you alive? Type true or false");
+      boolean clientAlive = scan.nextBoolean();
+      
+
+      // 3. Call mutator methods (setters) to update the object
+      newTrip.setTravelerName(clientName);
+      newTrip.setDestination(clientDestination);
+      newTrip.setDurationInHours(clientDuration);
+      newTrip.setIsAlive(clientAlive);
+      newTrip.setTotalCost();
+      
+
+      // 4. Return the completed travel object
+      return newTrip;
    }
-}
+
+
+} // end Main class
--- SubmarineTrip.java
+++ SubmarineTrip.java
@@ -9,15 +9,79 @@ private double totalCost;
 // 2. CONSTRUCTORS (default + parameterized)
 public SubmarineTrip() {
     this.travelerName = "DefaultClient";
-    this.destination = "Titanic Wreck"
+    this.destination = "Titanic Wreck";
     this.durationInHours =8;
     this.isAlive = false;
-    this.totalCost.processFee * durationInDays; 
+    this.totalCost = processFee * durationInHours;
 }
 public SubmarineTrip(String name, String destination, int duration, boolean alive) {
     this.travelerName = name;
-    this.totalCost = processFee * this.durationInDays;
+    this.destination = destination;
+    this.durationInHours = duration;
+    this.isAlive = alive;
+    this.totalCost = processFee * durationInHours;
 }
+
+// GETTERS
+public String getTravelerName() {
+    return travelerName;
+}
+
+public String getDestination() {
+    return destination;
+}
+
+public int getDurationInHours() {
+    return durationInHours;
+}
+
+public boolean getIsAlive() {
+    return isAlive;
+}
+
+public double getTotalCost() {
+    return totalCost;
+}
+
+public static int getProcessFee() {
+    return processFee;
+}
+
+// SETTERS
+public void setTravelerName(String travelerName) {
+    this.travelerName = travelerName;
+}
+
+public void setDestination(String destination) {
+    this.destination = destination;
+}
+
+public void setDurationInHours(int durationInHours) {
+    this.durationInHours = durationInHours;
+}
+
+public void setIsAlive(boolean isAlive) {
+    this.isAlive = isAlive;
+}
+
+public void setTotalCost() {
+    this.totalCost = processFee * this.durationInHours;
+}
+ public String toString() {
+    String tripDetails = "-----------------------------------------\n" +
+    "     SUBMARINE TRIP ITINERARY    \n" +
+    "-----------------------------------------\n" +
+    "TRAVELER NAME:  " + travelerName + "\n" +
+    "DESTINATION:    " + destination + "\n" +
+    "DURATION:       " + durationInHours + " Hours\n" +
+    "STATUS:         " + (isAlive ? "Alive" : "Not Alive") + "\n" +
+    "-----------------------------------------\n" +
+    "TOTAL COST:   $" + totalCost + "\n" +
+    "-----------------------------------------\n" +
+    "  Thank you for choosing DeepSea Tours!  \n";
+    return tripDetails;
+}
+
 
 
 }
